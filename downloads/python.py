079a160c836b096fb3b73950c70eb8ec4b7326f1
Arina <arinnee.mscgmail.com@MacBook-Air-Arina.local>
2025-06-06 17:17:57 +0200

    🔧 Обновления: новый RDF, XSLT, HTML, CSV-файлы, Python скрипт

--- downloads/python.py
+++ downloads/python.py
@@ -1,66 +1,82 @@
-
 from rdflib import Graph, Literal, Namespace, RDF, URIRef
 from rdflib.namespace import DC, XSD
 import pandas as pd
 import os
 
-# Базовые пространства имен
 SCHEMA = Namespace("http://schema.org/")
 LIDO = Namespace("http://www.lido-schema.org/")
+WD = Namespace("http://www.wikidata.org/entity/")
 BASE = "http://brodskiy-lod.github.io/"
 
-# Инициализация графа
 g = Graph()
 g.bind("dc", DC)
 g.bind("xsd", XSD)
 g.bind("schema", SCHEMA)
 g.bind("lido", LIDO)
+g.bind("wd", WD)
 
 def add_item(subject_id, data):
     uri = URIRef(BASE + subject_id)
-    g.add((uri, DC.title, Literal(data.get("title"), datatype=XSD.string)))
-    g.add((uri, DC.creator, Literal(data.get("creator"), datatype=XSD.string)))
-    g.add((uri, DC.date, Literal(data.get("date"), datatype=XSD.gYear)))
-    g.add((uri, DC.description, Literal(data.get("description"), datatype=XSD.string)))
-    g.add((uri, DC.subject, Literal(data.get("subject"), datatype=XSD.string)))
-    g.add((uri, RDF.type, Literal(data.get("type"), datatype=XSD.string)))
+    
+    if "title" in data:
+        g.add((uri, DC.title, Literal(data["title"], datatype=XSD.string)))
+    if "creator" in data:
+        creator = data["creator"]
+        g.add((uri, DC.creator, wd_or_literal(creator)))
+    if "date" in data:
+        g.add((uri, DC.date, Literal(data["date"], datatype=XSD.gYear)))
+    if "description" in data:
+        g.add((uri, DC.description, Literal(data["description"], datatype=XSD.string)))
+    if "type" in data:
+        g.add((uri, RDF.type, wd_or_literal(data["type"])))
 
-    if "location" in data:
-        g.add((uri, SCHEMA.location, Literal(data.get("location"), datatype=XSD.string)))
-    if "language" in data:
-        g.add((uri, DC.language, Literal(data.get("language"), datatype=XSD.string)))
-    if "format" in data:
-        g.add((uri, DC.format, Literal(data.get("format"), datatype=XSD.string)))
-    if "genre" in data:
-        g.add((uri, SCHEMA.genre, Literal(data.get("genre"), datatype=XSD.string)))
-    if "locationCreated" in data:
-        g.add((uri, SCHEMA.locationCreated, Literal(data.get("locationCreated"), datatype=XSD.string)))
+
+    for field, predicate in [
+        ("subject", DC.subject),
+        ("language", DC.language),
+        ("format", DC.format),
+        ("genre", SCHEMA.genre),
+        ("location", SCHEMA.location),
+        ("locationCreated", SCHEMA.locationCreated),
+        ("material", SCHEMA.material),
+        ("function", SCHEMA.function),
+        ("publisher", DC.publisher),
+        ("inscription", LIDO.inscriptionTranscription),
+        ("identifier", DC.identifier),
+        ("dateCreated", SCHEMA.dateCreated),
+        ("duration", SCHEMA.duration)
+    ]:
+        if field in data:
+            values = ensure_list(data[field])
+            for val in values:
+                g.add((uri, predicate, wd_or_literal(val)))
+
     if "relatedTo" in data:
-        for item in data["relatedTo"]:
-            g.add((uri, SCHEMA.relatedTo, Literal(item, datatype=XSD.string)))
-    if "inscription" in data:
-        g.add((uri, LIDO.inscriptionTranscription, Literal(data.get("inscription"), datatype=XSD.string)))
+        for rel in data["relatedTo"]:
+            g.add((uri, SCHEMA.relatedTo, URIRef(BASE + rel)))
 
-# Пример структуры данных
-items = [
-    {
-        "subject": "Brodsky_Tombstone",
-        "title": "Joseph Brodsky’s tombstone",
-        "creator": "Vladimir Radunsky",
-        "date": "1997",
-        "description": "A tombstone created by Vladimir Radunsky in memory of Joseph Brodsky.",
-        "subject": "Joseph Brodsky",
-        "type": "Memorial",
-        "location": "Venice",
-        "relatedTo": ["Memorial_plaque", "Photo_Brodsky_Venice"],
-        "inscription": "Iosif Brodsky 24.V.1940–28.1.1996"
-    },
-    # добавьте сюда остальные элементы по аналогии
-]
 
-# Генерация графа
-for item in items:
-    add_item(item["subject"], item)
+def wd_or_literal(value):
+    value = value.strip()
+    if value.startswith("Q"):
+        return WD[value]
+    return Literal(value, datatype=XSD.string)
 
-# Сохранение в файл
-g.serialize(destination="/mnt/data/brodsky_rdf_script_output.ttl", format="turtle")
+def ensure_list(val):
+    if isinstance(val, list):
+        return val
+    if isinstance(val, str):
+        return [v.strip() for v in val.split(";") if v.strip()]
+    return []
+
+folder = "data/"
+for file in os.listdir(folder):
+    if file.endswith(".csv"):
+        df = pd.read_csv(os.path.join(folder, file))
+        for _, row in df.iterrows():
+            data = row.to_dict()
+            subject_id = data.pop("id", os.path.splitext(file)[0])
+            add_item(subject_id, data)
+
+
+g.serialize(destination="brodsky_final.ttl", format="turtle")
